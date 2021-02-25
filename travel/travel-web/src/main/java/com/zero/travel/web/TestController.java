34b42a969c6ba9ed89dd7aed29548ae4155e08db
Nobody-LC <lemon_ljc@126.com>
2021-02-25 22:13:45 +0800

    测试页面跳转

--- travel/travel-web/src/main/java/com/zero/travel/web/TestController.java
+++ travel/travel-web/src/main/java/com/zero/travel/web/TestController.java
@@ -1,7 +1,10 @@
 package com.zero.travel.web;
 
 import com.zero.travel.pojo.dto.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -12,12 +15,12 @@ import java.util.List;
  * @version 1.0
  * @date 2021/2/25 12:06
  */
-@RestController
+@Controller
 @RequestMapping("/test")
 public class TestController {
 
     @RequestMapping("/response")
-    public List<User> test(){
+    public @ResponseBody List<User> test(){
         List<User> list = new ArrayList<>();
         list.add(new User("Jack",19));
         list.add(new User("Tom",25));
@@ -25,4 +28,18 @@ public class TestController {
 
         return list;
     }
+
+    @RequestMapping("/page")
+    public String  testPage(ModelMap map){
+        List<String> list = new ArrayList<>();
+        list.add("Jack");
+        list.add("Tom");
+        list.add("Alice");
+
+        map.addAttribute("lists",list);
+
+        return "Test";
+    }
+
+
 }
