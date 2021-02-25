3aa26c1875bd3fc79142c4c90117e34a295dbd77
Nobody-LC <lemon_ljc@126.com>
2021-02-25 21:05:08 +0800

    整合MyBatis

---
+++ travel/travel-mapper/src/main/java/com/zero/travel/config/MyBatisConfig.java
@@ -0,0 +1,25 @@
+package com.zero.travel.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * @author LJC
+ * @version 1.0
+ * @date 2021/2/25 20:51
+ */
+@Configuration
+@MapperScan("com.zero.travel.mapper")
+public class MyBatisConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource(){
+        return new DruidDataSource();
+    }
+}
