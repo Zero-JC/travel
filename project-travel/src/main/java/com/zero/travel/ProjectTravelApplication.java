cae9657ae32ecfdca5cc07519d207264dbf373e4
Nobody-LC <lemon_ljc@126.com>
2021-02-26 22:38:07 +0800

    单体模块项目

---
+++ project-travel/src/main/java/com/zero/travel/ProjectTravelApplication.java
@@ -0,0 +1,16 @@
+package com.zero.travel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author LJC
+ */
+@SpringBootApplication
+public class ProjectTravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectTravelApplication.class, args);
+    }
+
+}
