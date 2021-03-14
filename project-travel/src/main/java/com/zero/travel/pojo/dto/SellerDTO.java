3609d571898fef602349f0818af59e8ba522c609
Nobody-LC <lemon_ljc@126.com>
2021-03-14 21:05:35 +0800

    商家管理(表单校验优化)

--- project-travel/src/main/java/com/zero/travel/pojo/dto/SellerDTO.java
+++ project-travel/src/main/java/com/zero/travel/pojo/dto/SellerDTO.java
@@ -16,14 +16,14 @@ import java.io.Serializable;
 @Data
 public class SellerDTO implements Serializable {
 
-    @Min(value = 0,message = "商家编号格式不正确(2~4个字符,仅限数组)")
+    @Min(value = 0,message = "商家编号格式不正确")
     private Integer sellerId;
 
     @NotBlank(message = "商家名称不能为空")
     private String sellerName;
 
     @NotBlank(message = "联系电话不能为空")
-    @Pattern(regexp = "^[0-9]{11}$",message = "联系电话格式不正确(11个数字字符)")
+    @Pattern(regexp = "^[0-9]{4,16}$",message = "联系电话格式不正确(4~16位数字字符)")
     private String phone;
 
     @NotBlank(message = "商家地址不能为空")
