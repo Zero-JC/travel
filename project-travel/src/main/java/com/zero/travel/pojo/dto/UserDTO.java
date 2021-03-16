f5414b100181f7cd410f59b7c356546d76b66bef
Nobody-LC <lemon_ljc@126.com>
2021-03-16 22:35:46 +0800

    客户管理(客户数据传输对象)

---
+++ project-travel/src/main/java/com/zero/travel/pojo/dto/UserDTO.java
@@ -0,0 +1,27 @@
+package com.zero.travel.pojo.dto;
+
+import lombok.Data;
+
+/**
+ * 客户数据传输对象
+ * @author LJC
+ * @version 1.0
+ * @date 2021/3/16 22:34
+ */
+@Data
+public class UserDTO {
+
+    private Integer userId;
+
+    private String name;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    private Integer status;
+
+    private String code;
+}
